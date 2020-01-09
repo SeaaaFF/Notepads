74ddc7c7b5386373e8c9e3f275d10d2c30780791
feng <846783735@qq.com>
2020-01-09 10:02:49 +0800

    Initial commit

---
+++ app/src/main/java/notepad/itcast/cn/notepad/NotepadActivity.java
@@ -0,0 +1,102 @@
+package notepad.itcast.cn.notepad;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import notepad.itcast.cn.notepad.adapter.NotepadAdapter;
+import notepad.itcast.cn.notepad.bean.NotepadBean;
+import notepad.itcast.cn.notepad.database.SQLiteHelper;
+
+
+public class NotepadActivity extends AppCompatActivity {
+    ListView listView;
+    List<NotepadBean> list;
+    SQLiteHelper mSQLiteHelper;
+    NotepadAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notepad);
+        listView = (ListView) findViewById(R.id.listview);
+        ImageView add = (ImageView) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotepadActivity.this,RecordActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+        initData();
+    }
+    protected void initData() {
+        mSQLiteHelper = new SQLiteHelper(this);
+        showQueryData();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                NotepadBean notepadBean = list.get(position);
+                Intent intent = new Intent(NotepadActivity.this, RecordActivity.class);
+                intent.putExtra("id", notepadBean.getId());
+                intent.putExtra("time", notepadBean.getNotepadTime());
+                intent.putExtra("content", notepadBean.getNotepadContent());
+                NotepadActivity.this.startActivityForResult(intent, 1);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog dialog;
+                AlertDialog.Builder builder = new AlertDialog.Builder(NotepadActivity.this)
+                        .setMessage("是否删除此记录")
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                NotepadBean notepadBean = list.get(position);
+                                if (mSQLiteHelper.deleteData(notepadBean.getId())) {
+                                    list.remove(position);
+                                    adapter.notifyDataSetChanged();
+                                    Toast.makeText(NotepadActivity.this, "删除成功", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                dialog = builder.create();
+                dialog.show();
+                return true;
+            }
+        });
+
+
+
+        }
+    protected void showQueryData(){
+        if(list!=null){
+            list.clear();
+        }
+        list = mSQLiteHelper.query();
+        adapter = new NotepadAdapter(this,list);
+        listView.setAdapter(adapter);
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==1&&resultCode==2){
+            showQueryData();
+        }
+    }
+}
---
+++ app/src/main/java/notepad/itcast/cn/notepad/RecordActivity.java
@@ -0,0 +1,105 @@
+package notepad.itcast.cn.notepad;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import notepad.itcast.cn.notepad.database.SQLiteHelper;
+import notepad.itcast.cn.notepad.utils.DBUtils;
+
+public class RecordActivity extends Activity implements View.OnClickListener {
+    ImageView note_back;
+    TextView note_time;
+    EditText content;
+    ImageView delete;
+    ImageView note_save;
+    SQLiteHelper mSQLiteHelper;
+    TextView noteName;
+    String id;
+
+    @Override
+    protected void onCreate(Bundle savedInseanceState) {
+        super.onCreate(savedInseanceState);
+        setContentView(R.layout.activity_record);
+        note_back = (ImageView) findViewById(R.id.note_back);
+        note_time = (TextView) findViewById(R.id.tv_time);
+        content = (EditText) findViewById(R.id.note_content);
+        delete = (ImageView) findViewById(R.id.delete);
+        note_save = (ImageView) findViewById(R.id.note_save);
+        noteName = (TextView) findViewById(R.id.note_name);
+        note_back.setOnClickListener(this);
+        delete.setOnClickListener(this);
+        note_save.setOnClickListener(this);
+        initData();
+
+    }
+
+    protected void initData() {
+        mSQLiteHelper = new SQLiteHelper(this);
+        noteName.setText("添加记录");
+        Intent intent=getIntent();
+        if (intent!=null){
+            id=intent.getStringExtra("id");
+            if (id!=null){
+                noteName.setText("修改记录");
+                content.setText(intent.getStringExtra("content"));
+                note_time.setText(intent.getStringExtra("time"));
+                note_time.setVisibility(View.VISIBLE);
+            }
+        }
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.note_back:
+                finish();
+                break;
+            case R.id.delete:
+                content.setText("");
+                break;
+            case R.id.note_save:
+                String noteContent = content.getText().toString().trim();
+                if (id != null) {
+                    if (noteContent.length() > 0) {
+                        if (mSQLiteHelper.updateData(id, noteContent, DBUtils.getTime())) {
+                            showToast("修改成功");
+                            setResult(2);
+                            finish();
+                        } else {
+                            showToast("修改失败");
+                        }
+                    } else {
+                        showToast("修改内容不能为空");
+                    }
+                } else {
+                    if (noteContent.length() > 0) {
+                        if (mSQLiteHelper.insertData(noteContent, DBUtils.getTime())){
+                            showToast("保存成功");
+                            setResult(2);
+                            finish();
+                        } else {
+                            showToast("保存失败");
+                        }
+                    } else {
+                        showToast("修改内容不能为空!");
+                    }
+
+                }
+
+                break;
+        }
+
+    }
+
+    public void  showToast(String message) {
+        Toast.makeText(RecordActivity.this,message,Toast.LENGTH_SHORT).show();
+    }
+}
